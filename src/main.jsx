398943a18edc66bf54f347ab9bac1fae62419aaa
kangseungmin <ksmzaq@gmail.com>
2026-08-20 17:59:17 +0900

    style: 아이 등록 카드 수정
    - 원형 모양으로 변경
    - hover 시 plus 아이콘 표시

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SWRConfig } from "swr";
-import App from "./App";
-import { swrFetcher } from "@/api/apiClient";
 import "@/styles/variables.css";
 import "@/styles/global.css";
 import "@/styles/components.css";
+import App from "./App";
+import { swrFetcher } from "@/api/apiClient";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
